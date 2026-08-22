9ca91b1f3b595f923ccbb7e721fad8abbe57d44a
Aaron Dieter <aarondieter714@gmail.com>
2026-08-22 14:23:20 -0500

    Add navigation menu on front page

--- app.py
+++ app.py
@@ -9,16 +9,27 @@ st.set_page_config(
 
 st.title("🥁 UW Percussion Practice Rooms")
 
-st.write("Practice room dashboard")
+st.write("Select a page from the navigation bar or below to view the live schedule, occupancy predictions, or statistics.")
 
-rooms = ["Studio", "1401", "1407", "1409", "1413", "1414", "1416", "1417", "1418"]
+# rooms = ["Studio", "1401", "1407", "1409", "1413", "1414", "1416", "1417", "1418"]
 
-# live_schedule = st.Page("pages/1_live-schedule.py")
-# occupancy_predictions = st.Page("pages/2_occupancy-predictions.py")
+live_schedule_page = st.Page("pages/1_Live_Schedule.py")
+occupancy_predictions_page = st.Page("pages/2_Occupancy_Predictions.py")
+statistics_page = st.Page("pages/3_Statistics.py")
 
+pg = st.navigation([live_schedule_page, occupancy_predictions_page, statistics_page])
 
-# pg = st.navigation([live_schedule, occupancy_predictions])
-# pg.run()
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.page_link(live_schedule_page) # , label="Home", icon="📅"
+with col2:
+    st.page_link(occupancy_predictions_page) # , label="Dashboard", icon="🎯"
+with col3:
+    st.page_link(statistics_page) # , label="Statistics", icon="📊"
+
+st.divider()
+
+pg.run()
 
 
 
